0532b43b714925f7b638b4684974ab1107358227
John Doughty <jrdoughty13@gmail.com>
2024-11-17 12:13:29 -0600

    made hunger not specific to the single satyr and placed a second one

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -2,27 +2,30 @@ namespace CSGoap
 {
     using Godot;
     using System;
+    using System.Collections.Generic;
 
     public partial class Main : Node2D
     {
         private ProgressBar hungerField;
-        public GoapAgent agent;
+        public List<GoapAgent> agents;
 
         public override void _Ready()
         {
             hungerField = GetNode<ProgressBar>("HUD/VBoxContainer/MarginContainer/HBoxContainer/hunger");
-            agent = GetNode<Satyr>("satyr").agent;
+            Godot.Collections.Array<Node> nodes = GetTree().GetNodesInGroup("satyr");
+            agents = new List<GoapAgent>();
+            foreach (Satyr node in nodes)
+            {
+                agents.Add((node as Satyr).agent as GoapAgent);
+            }
         }
 
         private void OnHungerTimerTimeout()
         {
-            hungerField.Value = Convert.ToInt32(agent.GetState("hunger", 0));
-            if (hungerField.Value < 100)
+            foreach (var agent in agents)
             {
-                hungerField.Value += 2;
+                agent.SetState("hunger", Convert.ToDouble(agent.GetState("hunger", 0)) + 4);
             }
-
-            agent.SetState("hunger", hungerField.Value);
         }
 
         private void OnReloadPressed()
